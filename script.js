4c59f654eedd4b874d77b68eff2c32e6617ffd75
ES_PREPA_DEV <setifeliott@gmail.com>
2025-10-20 10:16:59 +0200

    Mise en place du JS pour attendre le chargement du DOM

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Script loaded");
+/*<div class="card">
+                <!--div.card-back+div.card-img -->
+                <div class="card-back"></div>
+                <div class="card-img" style="background-image:url('img/1.png')"></div>
+            </div>
+  */
+function handlerDomContentLoaded() {  
+    //Le code du jeu 
+}
+
+//Mise ne place d'un ecouteur pour ne lancer le code que lorsque le navigateur a terminé de fabrgiquer le DOM
+document.addEventListener('DOMContentLoaded', handlerDomContentLoaded);
